9803db718992be42b72857860df36fc6e1deb755
shadowsje <71304450+shadowsje@users.noreply.github.com>
2020-12-01 11:49:45 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://shadowsje.github.io/frog10924101/model.json';
 
 
 function preload() {
